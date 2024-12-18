71b52447da3afe1583117df3fbd4c9c4a7b57e1a
justin8765-hub <as84@duck.com>
2024-12-18 17:17:33 +0000

    Update ChangeLog.cs

--- ChangeLog.cs
+++ ChangeLog.cs
@@ -1,38 +1,28 @@
-CREATE PROCEDURE dbo.GetUserFieldChangesAsJson
-    @UserId INT,
-    @AuditDate DATETIME
+CREATE PROCEDURE GetChangeDetails
+    @TableID INT,
+    @ChangeDate DATETIME
 AS
 BEGIN
     SET NOCOUNT ON;
 
-    -- Fetch only the changes for the specific UserId and AuditDate
-    SELECT 
-        ua.AuditId,
-        ua.AuditDate,
-        ua.Operation,
-        JSON_QUERY(
-            CASE
-                WHEN ua.Operation = 'UPDATE' THEN 
-                    JSON_OBJECT(
-                        'UserName', 
-                        CASE WHEN ua.OldUserName <> ua.UserName THEN JSON_OBJECT('Old', ua.OldUserName, 'New', ua.UserName) ELSE NULL END,
-                        'Email', 
-                        CASE WHEN ua.OldEmail <> ua.Email THEN JSON_OBJECT('Old', ua.OldEmail, 'New', ua.Email) ELSE NULL END,
-                        'ModifiedDate', 
-                        CASE WHEN ua.OldModifiedDate <> ua.ModifiedDate THEN JSON_OBJECT('Old', CONVERT(VARCHAR, ua.OldModifiedDate, 120), 'New', CONVERT(VARCHAR, ua.ModifiedDate, 120)) ELSE NULL END
-                    )
-                WHEN ua.Operation = 'INSERT' THEN 
-                    JSON_OBJECT('InsertedValues', JSON_OBJECT('UserName', ua.UserName, 'Email', ua.Email))
-                WHEN ua.Operation = 'DELETE' THEN 
-                    JSON_OBJECT('DeletedValues', JSON_OBJECT('UserName', ua.UserName, 'Email', ua.Email))
-            END
-        ) AS ChangeDetails
-    FROM 
-        dbo.UserAudit ua
-    WHERE 
-        ua.UserId = @UserId
-        AND CAST(ua.AuditDate AS DATE) = CAST(@AuditDate AS DATE)
-    ORDER BY 
-        ua.AuditDate DESC
-    FOR JSON PATH;
-END;
+    -- Generate JSON of changes between previous and current states
+    SELECT (
+        '{ "TableId": ' + CAST(@TableID AS VARCHAR) + ', "ChangeDetails": {' +
+        STRING_AGG(
+            '"' + COLUMN_NAME + '": {"Current": "' + ISNULL(CAST(CurrentValue AS NVARCHAR), 'NULL') + '", "Previous": "' + ISNULL(CAST(PreviousValue AS NVARCHAR), 'NULL') + '"}', ', ') +
+        '} }'
+    ) AS JSON_Result
+    FROM (
+        SELECT
+            a.COLUMN_NAME,
+            b.VALUE AS PreviousValue,
+            c.VALUE AS CurrentValue
+        FROM
+            (SELECT * FROM TableAudit WHERE TableID = @TableID AND ChangeDate < @ChangeDate ORDER BY ChangeDate DESC LIMIT 1) b
+        FULL OUTER JOIN
+            (SELECT * FROM TableAudit WHERE TableID = @TableID AND ChangeDate = @ChangeDate) c
+        ON b.COLUMN_NAME = c.COLUMN_NAME
+    ) Changes
+    WHERE ISNULL(PreviousValue, 'NULL') <> ISNULL(CurrentValue, 'NULL');
+
+END
