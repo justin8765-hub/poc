30ceed9e45e998657ec8139bc651b5e174b9ae4d
justin8765-hub <as84@duck.com>
2024-12-18 17:43:39 +0000

    Update ChangeLog.cs

--- ChangeLog.cs
+++ ChangeLog.cs
@@ -5,24 +5,32 @@ AS
 BEGIN
     SET NOCOUNT ON;
 
-    -- Generate JSON of changes between previous and current states
+    -- Generate JSON of changes dynamically
     SELECT (
         '{ "TableId": ' + CAST(@TableID AS VARCHAR) + ', "ChangeDetails": {' +
         STRING_AGG(
-            '"' + COLUMN_NAME + '": {"Current": "' + ISNULL(CAST(CurrentValue AS NVARCHAR), 'NULL') + '", "Previous": "' + ISNULL(CAST(PreviousValue AS NVARCHAR), 'NULL') + '"}', ', ') +
+            '\"' + col.COLUMN_NAME + '\": {\"Current\": \"' + ISNULL(CAST(cs.VALUE AS NVARCHAR), 'NULL') + '\", \"Previous\": \"' + ISNULL(CAST(ps.VALUE AS NVARCHAR), 'NULL') + '\"}', ', ') +
         '} }'
     ) AS JSON_Result
     FROM (
-        SELECT
-            a.COLUMN_NAME,
-            b.VALUE AS PreviousValue,
-            c.VALUE AS CurrentValue
-        FROM
-            (SELECT * FROM TableAudit WHERE TableID = @TableID AND ChangeDate < @ChangeDate ORDER BY ChangeDate DESC LIMIT 1) b
-        FULL OUTER JOIN
-            (SELECT * FROM TableAudit WHERE TableID = @TableID AND ChangeDate = @ChangeDate) c
-        ON b.COLUMN_NAME = c.COLUMN_NAME
-    ) Changes
-    WHERE ISNULL(PreviousValue, 'NULL') <> ISNULL(CurrentValue, 'NULL');
-
+        -- Get the previous state
+        SELECT * 
+        FROM TableAudit 
+        WHERE TableID = @TableID AND ChangeDate < @ChangeDate
+        ORDER BY ChangeDate DESC
+        OFFSET 0 ROWS FETCH NEXT 1 ROWS ONLY
+    ) ps
+    FULL OUTER JOIN (
+        -- Get the current state
+        SELECT * 
+        FROM TableAudit 
+        WHERE TableID = @TableID AND ChangeDate = @ChangeDate
+    ) cs
+    ON 1=1 -- Compare all columns dynamically
+    CROSS APPLY (
+        SELECT COLUMN_NAME, ps.VALUE AS PreviousValue, cs.VALUE AS CurrentValue
+        FROM INFORMATION_SCHEMA.COLUMNS col
+        WHERE col.TABLE_NAME = 'TableAudit'
+    ) col
+    WHERE ISNULL(ps.VALUE, 'NULL') <> ISNULL(cs.VALUE, 'NULL');
 END
